455b9ffd4187cb277cb30529336a3f4ab6814e84
alisiared <134365023+alisiared@users.noreply.github.com>
2023-05-30 13:49:28 +0500

    Update test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -2,8 +2,12 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+link = "http://selenium1py.pythonanywhere.com/"
+
+def go_to_login_page(browser):
+    link = browser.find_element_by_css_selector("#login_link")
+    link.click()
+
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+   browser.get(link)
+   go_to_login_page(browser)
