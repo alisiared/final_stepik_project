69bd1d4039922b30bca1d4a1e3c247dd19c830d2
alisiared <134365023+alisiared@users.noreply.github.com>
2023-05-31 12:43:27 +0500

    поправила поиск url login_page.py

--- login_page.py
+++ login_page.py
@@ -8,7 +8,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert "/login" in self.url(), "login is absent in current url"
+        assert "login" in self.browser.current_url
         # реализуйте проверку на корректный url адрес
         
 
