9f61efcf3f2495e8190a95ec9241c16f329ce735
alisiared <134365023+alisiared@users.noreply.github.com>
2023-05-31 10:32:56 +0500

    добавлен тест ссылки на авторизацию test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,14 @@
-import pytest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
 
-link = "http://selenium1py.pythonanywhere.com/"
-
-def go_to_login_page(browser):
-    link = browser.find_element_by_css_selector("#login_link")
-    link.click()
 
 def test_guest_can_go_to_login_page(browser):
-   browser.get(link)
-   go_to_login_page(browser)
+    url = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, url)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.go_to_login_page() 
+    
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
