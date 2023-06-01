12840fa1500da1715a239984727d15d80c33b91e
alisiared <134365023+alisiared@users.noreply.github.com>
2023-06-01 15:18:01 +0500

    улучшение дизайна тестов

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,7 @@
 from .pages.product_page import ProductPage
 import pytest
 
-product_link_base = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+'''product_link_base = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 # links = [f"{product_link_base}/?promo=offer{number}" for number in range(10)]
 links = [pytest.param(f"{product_link_base}/?promo=offer{number}",
                       marks=pytest.mark.xfail(reason="плохой номер")) for number in range(10)]
@@ -13,5 +13,36 @@ def test_guest_can_add_product_to_basket(browser, url):
     page = ProductPage(browser, url)
     page.open()
     page.guest_add_product_to_basket()
-   
+
+@pytest.mark.xfail(reason="fixing this bug right now")
+def test_should_not_be_success_message(browser):
+    url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, url, 0)
+    page.open()
+    page.should_not_be_success_message()
     
+@pytest.mark.xfail(reason="fixing this bug right now")
+def test_success_message_is_disappeared(browser):
+    url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, url, 0)
+    page.open()
+    page.success_message_is_disappeared()
+    
+@pytest.mark.xfail(reason="fixing this bug right now")
+def test_success_message_is_not_presented(browser):
+    url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, url, 0)
+    page.open()
+    page.success_message_is_not_presented()'''
+    
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
