3eac7878f6029f736d8dba90184c20bf61ee7115
alisiared <134365023+alisiared@users.noreply.github.com>
2023-06-01 15:17:23 +0500

    улучшение дизайна тестов

--- product_page.py
+++ product_page.py
@@ -4,13 +4,17 @@ from .base_page import BasePage
 from .locators import ProductPageLocators
 import math
 
+
 class ProductPage(BasePage):
+    
+        
     def guest_add_product_to_basket(self):
         self.guest_can_add_product_to_basket()
         self.solve_quiz_and_get_code()
         self.should_be_same_name()
         self.should_be_same_price()
         
+         
     def guest_can_add_product_to_basket(self):
         basket = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
         basket.click()
@@ -28,7 +32,8 @@ class ProductPage(BasePage):
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
-            
+
+         
     def should_be_same_name(self):
         alertinner_name = self.browser.find_element(*ProductPageLocators.BASKET_ALERTINNER_NAME)
         name = self.browser.find_element(*ProductPageLocators.BOOK_NAME)
@@ -38,3 +43,19 @@ class ProductPage(BasePage):
         alertinner_price = self.browser.find_element(*ProductPageLocators.BASKET_ALERTINNER_PRICE)
         price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE)
         assert alertinner_price.text == price.text, "Price are not equal"
+        
+    def success_message_is_not_presented(self):
+        basket = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
+        basket.click()
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is not presented"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(
+            *ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, but should not be"
+
+    def success_message_is_disappeared(self):
+        basket = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
+        basket.click()
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is not disappeared, but should"
+
+    
