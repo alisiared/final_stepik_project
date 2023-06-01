35a3a5e95cbb5538b1f53cb8897e81a9c3050f03
alisiared <134365023+alisiared@users.noreply.github.com>
2023-06-01 16:19:58 +0500

    наследование и отрицательные проверки

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 from .pages.product_page import ProductPage
 import pytest
+from .pages.basket_page import BasketPage
 
 '''product_link_base = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 # links = [f"{product_link_base}/?promo=offer{number}" for number in range(10)]
@@ -33,7 +34,7 @@ def test_success_message_is_not_presented(browser):
     url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     page = ProductPage(browser, url, 0)
     page.open()
-    page.success_message_is_not_presented()'''
+    page.success_message_is_not_presented()
     
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
@@ -45,4 +46,11 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
-    page.go_to_login_page()
+    page.go_to_login_page()'''
+
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.guest_cant_see_product_in_basket_opened_from_product_page()
