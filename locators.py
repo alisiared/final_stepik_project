c2f317efaba5654751b7b180057a9b73755eca61
alisiared <134365023+alisiared@users.noreply.github.com>
2023-06-01 10:39:46 +0500

    добавлены новые локаторы для корзины

--- locators.py
+++ locators.py
@@ -7,3 +7,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    
+class ProductPageLocators():
+    BASKET_BUTTON = (By.XPATH, '//button[@class = "btn btn-lg btn-primary btn-add-to-basket"]')
+    BASKET_ALERTINNER_NAME = (By.CSS_SELECTOR, ".alertinner strong")
+    BASKET_ALERTINNER_PRICE = (By.CSS_SELECTOR, ".alertinner p strong")
+    BOOK_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    BOOK_PRICE = (By.CSS_SELECTOR, ".product_main p.price_color")
