821e61c35987ce2123dc8dba0a103ea423296206
alisiared <134365023+alisiared@users.noreply.github.com>
2023-06-01 16:19:25 +0500

    наследование и отрицательные проверки

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,9 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 
 
-def test_guest_can_go_to_login_page(browser):
+'''def test_guest_can_go_to_login_page(browser):
     url = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, url)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
@@ -14,5 +15,10 @@ def test_guest_should_see_login_link(browser):
     url = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, url)
     page.open()
-    page.should_be_login_link()
-    
+    page.should_be_login_link()'''
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    url = "http://selenium1py.pythonanywhere.com/"
+    page = BasketPage(browser, url)
+    page.open()
+    page.guest_cant_see_product_in_basket_opened_from_main_page()
