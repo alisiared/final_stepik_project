ee81a79c3b13dd8ad39df5e5ab73437b138c3969
alisiared <134365023+alisiared@users.noreply.github.com>
2023-06-02 14:52:09 +0500

    группировка тестов и setup

--- login_page.py
+++ login_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
-from .locators import LoginPageLocators
+from pages.locators import LoginPageLocators
+
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -20,3 +21,14 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
         # реализуйте проверку, что есть форма регистрации на странице
+
+    def register_new_user(self, email, password, c_password):
+        input_email = self.browser.find_element(*LoginPageLocators.EMAIL)
+        input_email.send_keys(email)
+        input_password = self.browser.find_element(*LoginPageLocators.PASSWORD)
+        input_password.send_keys(password)
+        input_c_password = self.browser.find_element(*LoginPageLocators.C_PASSWORD)
+        input_c_password.send_keys(c_password)
+        register_button = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
+        register_button.click()
+        
