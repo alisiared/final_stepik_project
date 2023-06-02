a2c0f040a0f1f3f29f5026f163e58871e8d6ca3e
alisiared <134365023+alisiared@users.noreply.github.com>
2023-06-02 14:51:37 +0500

    группировка тестов и setup

--- locators.py
+++ locators.py
@@ -1,12 +1,13 @@
 from selenium.webdriver.common.by import By
 
-
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    
+   
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    EMAIL = (By.CSS_SELECTOR, 'input[name="registration-email"]')
+    PASSWORD = (By.CSS_SELECTOR, 'input[name="registration-password1"]')
+    C_PASSWORD = (By.CSS_SELECTOR, 'input[name="registration-password2"]')
+    REGISTER_BUTTON = (By.NAME, "registration_submit")
     
 class ProductPageLocators():
     BASKET_BUTTON = (By.XPATH, '//button[@class = "btn btn-lg btn-primary btn-add-to-basket"]')
@@ -18,7 +19,8 @@ class ProductPageLocators():
 
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 class BasketPageLocators():
     BASKET_SEE_BUTTON = (By.XPATH, '//a[@class="btn btn-default"]')
